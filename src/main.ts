13b611675c81ad209edc910b32056eba4e1660c5
jnrpalma <nicolas.jnr10@outlook.com>
2024-06-06 13:52:02 -0300

    feat(login): ajuste em componente login

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { PoModule } from '@po-ui/ng-components'; 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(PoModule) 
   ]
 })
 .catch(err => console.error(err));
