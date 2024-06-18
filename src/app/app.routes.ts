17529f28ab63ecc52d159a054cb91af18038ab16
jnrpalma <nicolas.jnr10@outlook.com>
2024-06-18 09:37:32 -0300

    cria novo componente para listagem de lotes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterAnimalComponent } from './components/register-animal/register-animal.component';
 import { AllAnimalsComponent } from './components/all-animals/all-animals.component';
-import { OverviewComponent } from './components/overview/overview.component'; // Importe o componente Overview
+import { OverviewComponent } from './components/overview/overview.component';
+import { RegisterBatchComponent } from './components/register-batch/register-batch.component';
+import { AllBatchesComponent } from './components/all-batches/all-batches.component'; // Importe o novo componente
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [LoginRegisterGuard] },
@@ -14,7 +16,8 @@ export const routes: Routes = [
     path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], children: [
       { path: 'overview', component: OverviewComponent }, 
       { path: 'cadastrar', component: RegisterAnimalComponent },
-      { path: 'animais', component: AllAnimalsComponent } 
+      { path: 'animais', component: AllAnimalsComponent },
+      { path: 'lotes', component: AllBatchesComponent } // Adicione a nova rota aqui
     ]
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
