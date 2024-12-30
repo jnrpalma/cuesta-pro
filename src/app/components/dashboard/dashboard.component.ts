a2963196b989d3c416f5723d917e64cb02eaea64
jnrpalma <nicolas.jnr10@outlook.com>
2024-12-30 13:16:22 -0300

    feat(dashboard): ajusta modal

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,87 +1,66 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { PoButtonModule, PoChartModule, PoChartType, PoLoadingModule, PoAvatarModule, PoModalComponent } from '@po-ui/ng-components';
+import { PoButtonModule, PoModalComponent, PoModalModule, PoAvatarModule } from '@po-ui/ng-components';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule, PoChartModule, PoButtonModule, PoLoadingModule, PoAvatarModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    PoButtonModule,
+    PoModalModule,
+    PoAvatarModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
   @ViewChild('confirmExitModal') confirmExitModal!: PoModalComponent;
   @ViewChild('photoModal') photoModal!: PoModalComponent;
-  chartTypeLine: PoChartType = PoChartType.Line;
-  chartTypePie: PoChartType = PoChartType.Pie;
-  chartTypeColumn: PoChartType = PoChartType.Column;
-  chartTypeDonut: PoChartType = PoChartType.Donut;
-  chartTypeBar: PoChartType = PoChartType.Bar;
+
   userName: string = 'John Doe';
-  profileImage: string = ''; 
+  profileImage: string = '';
   isLoading: boolean = false;
-  isModalVisible = false;
-  isPhotoModalVisible: boolean = false;
   selectedPhoto: File | null = null;
+
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.authService.getUser().subscribe(user => {
       if (user) {
         this.userName = user.displayName || user.email;
-        this.profileImage = user.photoURL || ''; 
-        console.log('User name set to:', this.userName);
-        console.log('Profile image URL:', this.profileImage);
+        this.profileImage = user.photoURL || '';
       } else {
         this.userName = '';
-        this.profileImage = ''; 
+        this.profileImage = '';
       }
     });
   }
+
   openModal() {
-    this.isModalVisible = true;
+    this.confirmExitModal.open();
   }
 
   closeModal() {
-    this.isModalVisible = false;
+    this.confirmExitModal.close();
   }
-
-  confirmLogout() {
-    this.closeModal();
-    this.router.navigate(['/login']);
-  }
-
-  navigateTo(route: string) {
-    this.router.navigate([`/dashboard/${route}`]);
-  }
-  
-  logout() {
-    this.isLoading = true;
-    this.authService.logout().then(() => {
-      setTimeout(() => {
-        this.isLoading = false;
-        this.router.navigate(['/login']);
-      }, 1000);
-    });
-  }
-  
 
   openPhotoModal() {
-    this.isPhotoModalVisible = true;
+    this.photoModal.open();
   }
 
   closePhotoModal() {
-    this.isPhotoModalVisible = false;
+    this.photoModal.close();
   }
 
   onPhotoSelected(event: any) {
     const file = event.target.files[0];
     if (file) {
       this.selectedPhoto = file;
-      console.log('Foto selecionada:', file.name);
     }
   }
 
@@ -92,15 +71,27 @@ export class DashboardComponent implements OnInit {
         this.profileImage = reader.result as string;
         this.selectedPhoto = null;
         this.closePhotoModal();
-        console.log('Foto atualizada com sucesso!');
       };
       reader.readAsDataURL(this.selectedPhoto);
     }
   }
 
-  removePhoto() {
-    this.profileImage = '';
-    this.closePhotoModal();
-    console.log('Foto removida com sucesso!');
+  confirmLogout() {
+    this.closeModal();
+    this.logout();
+  }
+
+  logout() {
+    this.isLoading = true;
+    this.authService.logout().then(() => {
+      setTimeout(() => {
+        this.isLoading = false;
+        this.router.navigate(['/login']);
+      }, 1000);
+    });
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([`/dashboard/${route}`]);
   }
 }
