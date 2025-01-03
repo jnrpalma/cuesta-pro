6940d0c3315f0975d392ba3e63e7c784ed3bc9b8
jnrpalma <nicolas.jnr10@outlook.com>
2025-01-03 09:48:26 -0300

    feat(register): ajusta dica de senha para registro de usuario

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PoFieldModule, PoButtonModule, PoLoadingModule, PoLinkModule, PoAvatarModule, PoNotificationService } from '@po-ui/ng-components';
+import { PoFieldModule, PoButtonModule, PoLoadingModule, PoLinkModule, PoAvatarModule, PoTooltipModule, PoNotificationService } from '@po-ui/ng-components';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { ErrorHandleService } from '../../services/error-handle/error-handle.service';
@@ -9,7 +9,17 @@ import { ErrorHandleService } from '../../services/error-handle/error-handle.ser
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, PoFieldModule, PoButtonModule, PoLoadingModule, PoLinkModule, PoAvatarModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PoFieldModule,
+    PoButtonModule,
+    PoLoadingModule,
+    PoLinkModule,
+    PoAvatarModule,
+    PoTooltipModule
+  ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
@@ -29,6 +39,39 @@ export class RegisterComponent {
     private errorHandleService: ErrorHandleService
   ) {}
 
+  get passwordPattern(): string {
+    return this.password === this.confirmPassword ? '.*' : '^$';
+  }
+
+  isFormValid(): boolean {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return (
+      this.firstName.trim() !== '' &&
+      this.email.trim() !== '' &&
+      this.password.trim() !== '' &&
+      this.confirmPassword.trim() !== '' &&
+      this.password === this.confirmPassword &&
+      passwordRegex.test(this.password)
+    );
+  }
+
+  async onSubmit() {
+    if (this.isFormValid()) {
+      this.isLoading = true;
+      try {
+        const displayName = this.firstName;
+        await this.authService.register(this.email, this.password, displayName, this.profileImage);
+        this.poNotification.success('Registro realizado com sucesso!');
+      } catch (error) {
+        this.errorHandleService.handleRegistrationError(error); 
+      } finally {
+        this.isLoading = false;
+      }
+    } else {
+      this.poNotification.warning('Por favor, preencha todos os campos corretamente.');
+    }
+  }
+
   onFileChange(event: any) {
     const reader = new FileReader();
     if (event.target.files && event.target.files.length) {
@@ -44,23 +87,6 @@ export class RegisterComponent {
   triggerFileInput() {
     const fileInput = document.getElementById('fileInput') as HTMLInputElement;
     fileInput.click();
-  }
-
-  async onSubmit() {
-    if (this.firstName && this.email && this.password && this.password === this.confirmPassword) {
-      this.isLoading = true;
-      try {
-        const displayName = this.firstName;
-        await this.authService.register(this.email, this.password, displayName, this.profileImage);
-        this.poNotification.success('Registro realizado com sucesso!');
-      } catch (error) {
-        this.errorHandleService.handleRegistrationError(error); 
-      } finally {
-        this.isLoading = false;
-      }
-    } else {
-      this.poNotification.warning('Por favor, preencha todos os campos corretamente.');
-    }
   }
 
   goToLogin() {
